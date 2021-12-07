5757226bcd545a1fffb3d0f2a9e1586999674a6f
tusphananh <tusphananh@gmail.com>
2021-12-07 15:33:57 +0700

    first

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import "./App.css";
 import React from "react";
 import Unity, { UnityContext } from "react-unity-webgl";
-import styles from "./style.module.css";
+import styles from "./style.module.scss";
+import AceEditor from "react-ace";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlay } from "@fortawesome/free-solid-svg-icons";
+
+import "ace-builds/src-noconflict/mode-javascript";
+import "ace-builds/src-noconflict/theme-monokai";
+
 const unityContext = new UnityContext({
   loaderUrl: "./web/Build/web.loader.js",
   dataUrl: "./web/Build/web.data",
@@ -9,10 +16,11 @@ const unityContext = new UnityContext({
   codeUrl: "./web/Build/web.wasm",
 });
 
+function moveForward() {
+  unityContext.send("Character", "moveForward");
+}
+
 function App() {
-  function MoveForward() {
-    unityContext.send("Character", "moveForward");
-  }
   return (
     <div className={styles["container"]}>
       <div className={styles["background"]}></div>
@@ -21,6 +29,7 @@ function App() {
         <div className={styles["game-layout"]}>
           <p className={styles["happy-coding"]}>Happy Coding</p>
           <Unity
+            doNotCaptureKeyboard={true}
             className={styles["game-container"]}
             unityContext={unityContext}
           />
@@ -33,19 +42,48 @@ function App() {
 const GameController = () => {
   const [editorValue, setEditorValue] = React.useState("");
 
-  
+  function onChange(newValue) {
+    setEditorValue(newValue);
+  }
+
+  function onRun() {
+
+    moveForward();
+    try {
+      console.log(editorValue);
+      eval(editorValue);
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
   return (
     <div className={styles["game-controller"]}>
+      <div className={styles["game-controller__header"]}>
+        <p className={styles["game-controller-header__title"]}>
+          Game Controller
+        </p>
+        <button className={styles["game-controller__run-btn"]} onClick={onRun}>
+          <FontAwesomeIcon icon={faPlay} />
+        </button>
+      </div>
+      <div className={styles["lesson-container"]}></div>
       <div className={styles["editor-container"]}>
-        <textarea
-          className={styles["editor-area"]}
-          onChange={(e) => {
-            setEditorValue(e.target.value);
-          }}
-        >
-          {editorValue}
-        </textarea>
-        <div className={styles["preview"]}></div>
+        <AceEditor
+          placeholder="Type your code here"
+          mode="javascript"
+          theme="monokai"
+          className={styles["ace-editor"]}
+          onChange={onChange}
+          value={editorValue}
+          name="ace-editor"
+          editorProps={{ $blockScrolling: true }}
+          height="100%"
+          width="100%"
+          highlightActiveLine={false}
+          showGutter={false}
+          showPrintMargin={false}
+        />
       </div>
     </div>
   );
