d27ce792a5ccc3ac6a2ecf2dcdc7183d94ee2ae0
tusphananh <tusphananh@gmail.com>
2021-12-08 15:21:52 +0700

    update

--- src/App.js
+++ src/App.js
@@ -5,9 +5,10 @@ import styles from "./style.module.scss";
 import AceEditor from "react-ace";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlay, faSyncAlt } from "@fortawesome/free-solid-svg-icons";
-
+import { motion, useAnimation } from "framer-motion";
 import "ace-builds/src-noconflict/mode-javascript";
 import "ace-builds/src-noconflict/theme-monokai";
+import { Lesson } from "./Lesson";
 
 const unityContext = new UnityContext({
   loaderUrl: "./web/Build/web.loader.js",
@@ -25,9 +26,7 @@ window.rotateLeft = async () => {
 window.rotateRight = async () => {
   unityContext.send("Character", "rotateRight");
 };
-const getVictory = () => {
-  return unityContext.send("GameController", "getVictory");
-};
+
 const resetGame = () => {
   unityContext.send("GameController", "loadCurScene");
 };
@@ -43,27 +42,146 @@ const executeCode = (editorValue) => {
 };
 
 function App() {
+  const [isVictory, setIsVictory] = React.useState(false);
+  const [isScreenLoaded, setIsScreenLoaded] = React.useState(false);
+  const [sceneIdx, setSceneIdx] = React.useState(0);
+  const [progression, setProgression] = React.useState(0);
+  const animationControls = useAnimation();
+  const mainAnimationControls = useAnimation();
+
+  const playTransition = () => {
+    mainAnimationControls.start("mainShow");
+    animationControls.start("show");
+  };
+
+  React.useEffect(function () {
+    unityContext.on("GameOver", function (isVictory, isScreenLoaded, sceneIdx) {
+      setIsVictory(isVictory === 1);
+      setIsScreenLoaded(isScreenLoaded === 1);
+      setSceneIdx(sceneIdx);
+    });
+
+    unityContext.on("progress", function (progression) {
+      setProgression(progression);
+    });
+  }, []);
+  React.useEffect(
+    function () {
+      isVictory && isScreenLoaded && playTransition();
+    },
+    [isVictory, isScreenLoaded]
+  );
+
   return (
     <div className={styles["container"]}>
+      <Transition
+        animationControls={animationControls}
+        mainAnimationControls={mainAnimationControls}
+      />
+
       <div className={styles["background"]}></div>
       <div className={styles["content"]}>
-        <GameController />
+        <GameController
+          sceneIdx={sceneIdx}
+          playTransition={() => {
+            playTransition();
+          }}
+        />
         <div className={styles["game-layout"]}>
           <p className={styles["happy-coding"]}>Happy Coding</p>
-          <Unity
-            doNotCaptureKeyboard={true}
-            className={styles["game-container"]}
-            unityContext={unityContext}
-          />
+          <div className={styles["game-container"]}>
+            {progression < 1 && <ProgressPanel progression={progression} />}
+            <Unity
+              doNotCaptureKeyboard={true}
+              className={styles["game"]}
+              unityContext={unityContext}
+            />
+          </div>
         </div>
       </div>
     </div>
   );
 }
+const ProgressPanel = ({ progression }) => {
+  return (
+    //ClasName = "progress-bar blue stripes"
+    <div className={styles["progress-panel"]}>
+      <p>Loading {parseInt(progression * 100)}%</p>
+      <div
+        className={
+          styles["progress-bar"] +
+          " " +
+          styles["blue"] +
+          " " +
+          styles["stripes"]
+        }
+      >
+        <span style={{ width: progression * 100 + "%" }}></span>
+      </div>
+    </div>
+  );
+};
 
-const GameController = () => {
+const Transition = ({ animationControls, mainAnimationControls }) => {
+  const variants = {
+    mainShow: {
+      display: "block",
+      width: ["0%", "110%", "110%", "110%", "110%", "110%", "110%", "0%"],
+      transition: {
+        duration: 4,
+        ease: "easeInOut",
+      },
+      transitionEnd: {
+        display: "none",
+      },
+    },
+    show: {
+      width: ["0vw", "0vw", "0vw", "20vw", "30vw", "300vw", "110vw", "0vw"],
+      height: ["0vw", "0vw", "0vw", "20vw", "30vw", "300vw", "110vw", "0vw"],
+      top: [
+        "-0vw",
+        "-0vw",
+        "-0vw",
+        "-10vw",
+        "-30vw",
+        "-50vw",
+        "-30vw",
+        "-10vw",
+      ],
+      left: [
+        "-0vw",
+        "-0vw",
+        "-0vw",
+        "-10vw",
+        "-30vw",
+        "-50vw",
+        "-30vw",
+        "-10vw",
+      ],
+      transition: {
+        duration: 4,
+        ease: "easeInOut",
+      },
+    },
+  };
+  return (
+    <motion.div
+      className={styles["transition"]}
+      variants={variants}
+      animate={mainAnimationControls}
+    >
+      <motion.div
+        className={styles["transition__circle"]}
+        animate={animationControls}
+        variants={variants}
+      ></motion.div>
+    </motion.div>
+  );
+};
+const GameController = ({ sceneIdx, playTransition }) => {
   const [editorValue, setEditorValue] = React.useState("");
-
+  const [lessonValue, setLessonValue] = React.useState("");
+  const [lessonTitle, setLessonTitle] = React.useState("");
   function onChange(newValue) {
     setEditorValue(newValue);
   }
@@ -73,8 +191,16 @@ const GameController = () => {
   }
   function onReset() {
     resetGame();
-    console.log(getVictory());
+    playTransition();
   }
+
+  React.useEffect(
+    function () {
+      setLessonValue(Lesson[sceneIdx].content);
+      setLessonTitle(Lesson[sceneIdx].title);
+    },
+    [sceneIdx]
+  );
   return (
     <div className={styles["game-controller"]}>
       <div className={styles["game-controller__header"]}>
@@ -84,14 +210,12 @@ const GameController = () => {
         >
           <FontAwesomeIcon icon={faSyncAlt} />
         </button>
-        <p className={styles["game-controller-header__title"]}>
-          Game Controller
-        </p>
+        <p className={styles["game-controller-header__title"]}>{lessonTitle}</p>
         <button className={styles["game-controller__run-btn"]} onClick={onRun}>
           <FontAwesomeIcon icon={faPlay} />
         </button>
       </div>
-      <div className={styles["lesson-container"]}></div>
+      <div className={styles["lesson-container"]}>{lessonValue}</div>
       <div className={styles["editor-container"]}>
         <AceEditor
           placeholder="Type your code here"
---
+++ src/Lesson.js
@@ -0,0 +1,35 @@
+// eslint-disable-next-line no-sparse-arrays
+export const Lesson = [
+  {
+    id: 1,
+    title: "Function",
+    content:
+      "In this lesson, we will learn about function in javascript  \n Use these 3 functions: \n  1. moveForward() \n 2. rotateLeft()  \n 3. rotateRight() \n",
+    link: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+  },
+  {
+    id: 2,
+    title: "Lesson 2",
+    content: "This is the second lesson content",
+    link: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+  },
+  {
+    id: 3,
+    title: "Lesson 3",
+    content: "This is the third lesson content",
+    link: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+  },
+  ,
+  {
+    id: 4,
+    title: "Lesson 4",
+    content: "This is the fourth lesson content",
+    link: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+  },
+  {
+    id: 5,
+    title: "Lesson 5",
+    content: "This is the fifth lesson content",
+    link: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+  },
+];
